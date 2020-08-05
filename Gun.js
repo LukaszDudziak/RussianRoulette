6c919657a67fbf4a0894e244e89c0bc94229f83c
dudziakpudziak <lukaszdudziak777@gmail.com>
2020-08-05 21:00:16 +0200

    final_2085 - spin method in gun class

--- Gun.js
+++ Gun.js
@@ -3,9 +3,10 @@ class Gun {
     constructor() {
         this._bulletsNumber = document.querySelector(`input[name = BulletsNumber]:checked`).id;
         this.loadedCylinder = this.bulletsPosition(this._bulletsNumber);
-
+        // this.getLoadedCylinder = () => _loadedCylinder;
     }
 
+    //function which gonna create loaded cylinder with bullets in randon, unknown positions
     bulletsPosition = _bulletsNumber => {
         //creating empty cylinder
         let cylinder = [0, 0, 0, 0, 0, 0]
@@ -24,6 +25,19 @@ class Gun {
         return cylinder;
     }
 
-    spin
-
+    spin() {
+        //random number of spins, one spin can change position from 0 to 5 positions ahead
+        let spins = Math.floor(Math.random() * 6);
+        let loadedCylinder = this.loadedCylinder;
+        //i must create new variable, because overwritting "loadedCylinder" can cause situation, where existing bullet (1) will be overwritten by 0, and when it's posistion be checked, there will be 0 :| (dunno how to simplify this)
+        let spinnedCylinder = [];
+        console.log(spins)
+        for (let i = 0; i < loadedCylinder.length; i++) {
+            // console.log(loadedCylinder[(i + spins) % 6]);
+            // console.log(loadedCylinder[i]);
+            spinnedCylinder[(i + spins) % 6] = loadedCylinder[i];
+        }
+        //returning loaded revolver, ready for shot :D
+        return spinnedCylinder;
+    }
 }
