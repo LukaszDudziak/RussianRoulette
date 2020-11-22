c926ff1babe4163a85aa54cda3a42009b0bea926
dudziakpudziak <lukaszdudziak777@gmail.com>
2020-11-22 22:58:54 +0100

    html+css completly reworked / no js yet

--- main.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.gameStart').addEventListener('click', () => {
-    const game = new Game;
-}, {
-    //blocking user from creating more games in one attempt
-    once: true
-})
